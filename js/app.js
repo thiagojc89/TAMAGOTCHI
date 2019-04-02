95f635185e94fbb4d56d3c38c36e3f640ab2955c
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-04-02 16:18:58 -0500

    create the object game to call the functions

--- js/app.js
+++ js/app.js
@@ -1,45 +1,115 @@
 class Tamagotchi {
-	constructor(age){
+	constructor(name){
 		this.hunger = 1;
 		this.sleepness = 1;
 		this.boredom = 1;
-		this.age = null;
+		this.age = 0;
 		this.dead = false;
 		// this.ageTimer = 30;
+		this.name = name
 	}
-	startTimer() {
-		this.gameTimer = setInterval(() => {
+
+	getOlder() {
+		this.age++
+	}
+
+	// startTimer() {
+	// 	this.gameTimer = setInterval(() => {
     		
+ //    		console.log("timer running");
+    		
+ //    		//increase age 
+ //      		let $age = parseInt($('#age').text());
+ //      		console.log($age);
+ //      		$age+=1;
+ //      		$('#age').text($age);
+ //      		console.log()
+
+ //      		//the Timer will stop when the Tamagotchi is dead =[.
+ //      		if (this.dead){
+ //      			clearInterval(this.gameTimer)
+
+ //      		}
+      		
+ //    	// }, 60000)
+ //    	}, 1000)
+ //    	// this.feedPet = 
+	// }
+// }
+
+
+const game = {
+	myPet: null, 
+	intervalID: null,
+	time: 0,
+
+ 	start(name) {
+		// create new tomagotchi
+		const myPet2 = new Tamagotchi(name);
+		this.myPet = myPet2;
+		$('#name').text(this.myPet.name)
+		$('#input-name').val("")
+		
+		this.startTimer();
+
+
+	},
+	startTimer() {
+		this.intervalID = setInterval(() => {
+    		
+
+			this.time++
+			console.log(this.time);
+
+
     		console.log("timer running");
     		
     		//increase age 
-      		let $age = parseInt($('#age').text());
-      		console.log($age);
-      		$age+=1;
-      		$('#age').text($age);
-
+      		// let $age = parseInt($('#age').text());
+      		// console.log($age);
+      		this.myPet.getOlder();
+      		// $('#age').text($age);
+      		// console.log()
 
       		//the Timer will stop when the Tamagotchi is dead =[.
-      		if (this.dead){
-      			clearInterval(this.gameTimer)
-      		}
+      		// if (this.dead){
+      		// 	clearInterval(this.gameTimer)
+
+      		// }
       		
     	// }, 60000)
     	}, 1000)
+    	// this.feedPet = 
+	},
+
+	asdf() {
+
+
+	},
+	sdfg() {
+
 	}
 }
 
 
 
-const myPet = new Tamagotchi();
-myPet.startTimer();
+const fish = {
+	name: "fred",
+	swim() {
+		console.log(`hi i'm ${this.name} and I'm swimming`); // Hi I'm fred and I'm swimming
+	}
+}
 
- //while(my still alive)
 
-console.log(myPet.age);
-console.log(myPet.hunger);
-console.log(myPet.sleepness);
-console.log(myPet.boredom);
+
+// myPet.startTimer();
+
+//  //while(my still alive)
+
+// console.log(myPet.age);
+// console.log(myPet.hunger);
+// console.log(myPet.sleepness);
+// console.log(myPet.boredom);
 
 
 
@@ -54,9 +124,17 @@ console.log(myPet.boredom);
 $('#form').on('submit',(event)=>{
 	event.preventDefault()
 	const newName = $('#input-name').val()
-	$('#name').text(newName)
-	console.log("submit button clicked");
-	$('#input-name').val("")
+	// call a start method in your game object -- pass in newName
+	game.start(newName)
+
+
+
 
 
 })
+
+
+
+
+
+
